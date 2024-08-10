23fa56c71df9ba480d3725b1e3afcbd92c896f42
Vivian_ <sveta.ss2602@gmail.com>
2024-08-10 13:48:50 +0300

    fix(order-details): image output

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-// import img from '../../images/done.png';
+import img from '../../images/done.png';
 import Modal from '../modal/modal';
 import styles from './order-details.module.css';
 
@@ -8,7 +8,7 @@ function OrderDetails({ number, onClose }) {
         <Modal onClose={onClose}>
             <p className={`${styles["order-number"]} text text_type_digits-large mb-8`}>{number}</p>
             <p className="text text_type_main-medium mb-15 text-center">идентификатор заказа</p>
-            {/* <img src={img} className={`${styles.image} mb-15 text-center`} alt="Заказ принят" /> */}
+            <img src={img} className={`${styles.image} mb-15 text-center`} alt="Заказ принят" />
             <p className="text text_type_main-default mb-2 text-center">Ваш заказ начали готовить</p>
             <p className={`${styles["last-p"]} text text_type_main-default text_color_inactive mb-30 text-center`}>Дождитесь готовности на орбитальной станции</p>
         </Modal>
